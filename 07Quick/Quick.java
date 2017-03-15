77d57a9fc0caba69bf74bed285c7c437ee9259cb
William Hong <willhong20@gmail.com>
2017-03-15 18:08:30 -0400

    Improved part function to account for duplicates

--- 07Quick/Quick.java
+++ 07Quick/Quick.java
@@ -13,28 +13,28 @@ public class Quick{
 		System.out.println(partition); //for testing purposes only
 		
 		//finding # of larger elements than partition
-		int larger=0, i=start;
+		int largerOrEqual=0, i=start;
 		while(i < end){
-			if (data[i]  > partition){
-				larger += 1; }
+			if (data[i]  >= partition){
+				largerOrEqual += 1; }
 			i += 1;
 		}
 
 		//put the partition where it belongs
-		int swap1 = data[end-larger];
+		int swap1 = data[end-largerOrEqual];
 		data[randomIndex] = swap1;
-		data[end-larger] = partition;
+		data[end-largerOrEqual] = partition;
 		
 		//the last N elements must be occupied by the larger numbers
 		int goLeft = end, goRight = start;
-		while (goLeft > end-larger){
+		while (goRight < end-largerOrEqual && goLeft > end-largerOrEqual){
 			//this is when the element at the end belongs there
-			if (data[goLeft] > partition){
+			if (data[goLeft] >= partition){
 				goLeft -= 1; }
 			else{
 				//otherwise, make the swap with the elements in the front
 				//if applicable of course
-				if(data[goRight] > partition){
+				if(data[goRight] >= partition){
 					int greater = data[goRight];
 					data[goRight] = data[goLeft];
 					data[goLeft] = greater;
@@ -52,14 +52,16 @@ public class Quick{
 			x += 1; }
 		System.out.print("\n");
 		
-		return end-larger;
+		return end-largerOrEqual;
 	}
 	
 	public static void main (String[] args){
-		int[] test = new int[]{2, 4, 7, 9, 1, 6, 8, 5, 3, 0};
+		int[] test1 = new int[]{2, 4, 7, 9, 1, 6, 8, 5, 3, 0};
 		Quick x = new Quick();
-		//System.out.println(x.part(test, 0, 9));
-		System.out.println(x.part(test,0,9));
+		//System.out.println(x.part(test1,0,9));
+		
+		int[] test2 = new int[]{999,999,999,4,1,0,3,2,999,999,999};
+		System.out.println(x.part(test2,0,10));
 		
 	}
 	
