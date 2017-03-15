53ff6d2d72525ff72ef283f7cf9efc65b2f1dae8
William Hong <willhong20@gmail.com>
2017-03-15 19:29:48 -0400

    Completed quickselect function

--- 07Quick/Quick.java
+++ 07Quick/Quick.java
@@ -1,4 +1,4 @@
-import java.util.Random;
+import java.util.*;
 
 public class Quick{
 
@@ -10,7 +10,7 @@ public class Quick{
 	public static int part(int[] data, int start, int end){
 		int randomIndex = randomWithRange(start,end);
 		int partition = data[randomIndex];
-		System.out.println(partition); //for testing purposes only
+		//System.out.println(partition); //for testing purposes only
 		
 		//finding # of larger elements than partition
 		int largerOrEqual=0, i=start;
@@ -45,23 +45,66 @@ public class Quick{
 			}
 		}
 
-		//for testing purposes only
+		/*for testing purposes only
 		int x=0;
 		while (x < data.length){
 			System.out.print(data[x] + " ");
 			x += 1; }
-		System.out.print("\n");
+		System.out.print("\n"); */
 		
-		return end-largerOrEqual;
+		return end-largerOrEqual; 
 	}
 	
-	public static void main (String[] args){
-		int[] test1 = new int[]{2, 4, 7, 9, 1, 6, 8, 5, 3, 0};
-		Quick x = new Quick();
-		//System.out.println(x.part(test1,0,9));
+	public static int quickselect(int[] data, int k){
+		//first things first...
+		if (k < 0 || k >= data.length){
+			throw new IllegalArgumentException("Invalid k"); }
 		
-		int[] test2 = new int[]{999,999,999,4,1,0,3,2,999,999,999};
-		System.out.println(x.part(test2,0,10));
+		//these are the initial variables
+		int start = 0, end = data.length-1,
+			answer = part(data,start,end);
+		
+		//the variables will get updated in the while loop if 1st try doesn't work
+		while (answer != k){
+			//shrink the section of array we want to look at
+				
+			//choice 1: eliminate smaller choices
+			if (answer > k){
+				answer = part(data,k,end); }
+				
+			//choice 2: eliminate larger choices
+			else{
+				answer = part(data,start,k); }
+					
+		}
+		
+		return data[answer];
+	}
+	
+	//testing...
+	public static void main (String[] args){
+		Quick x = new Quick();
+		
+		/* int[] test3 = new int[]{};
+		System.out.println(quickselect(test3,0));//Invalid k
+		
+		int[] test1 = new int[]{2, 10, 15, 23, 0, 5};
+		System.out.println(quickselect(test1,0)); //0
+		System.out.println(quickselect(test1,1)); //2
+		System.out.println(quickselect(test1,2)); //5
+		System.out.println(quickselect(test1,3)); //10
+		System.out.println(quickselect(test1,4)); //15
+		System.out.println(quickselect(test1,5)); //23
+		
+		int[] test2 = new int[]{36, 8, 11, 55, 2, 13, 93, 20, 9, 76, 25, 90, 56, 10, 77,
+			66, 27, 64, 1, 33, 54, 70, 68, 43, 53, 98, 63, 91, 82, 83, 28, 74, 81, 32, 29, 
+			46, 85, 78, 99, 62, 30, 95, 24, 100, 52, 31, 17, 44, 87, 47, 41, 5, 89, 97, 40, 
+			38, 94, 15, 39, 4, 67, 49, 75, 72, 92, 22, 59, 57, 19, 65, 48, 37, 86, 26, 42, 
+			51, 73, 88, 7, 21, 34, 69, 79, 61, 45, 23, 18, 80, 50, 35, 16, 60, 84, 14, 58, 6, 3, 71, 96, 12};
+		System.out.println(quickselect(test2,0)); //1
+		System.out.println(quickselect(test2,49)); //50
+		System.out.println(quickselect(test2,99)); //100
+		System.out.println(quickselect(test2,100)); //Invalid k */
 		
 	}
 	
