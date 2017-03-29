0def1b871cd77a7de34d9fdcca96c13477b31937
William Hong <willhong20@gmail.com>
2017-03-29 00:51:31 -0400

    Only added the inner class

---
+++ 09LinkedList/LNode.java
@@ -0,0 +1,14 @@
+public class LNode{
+
+	private int value;
+	private LNode next;
+
+	//2 constructors required
+	public LNode(int myValue){
+		value = myValue; }
+
+	public LNode(int myValue, LNode myNext){
+		value = myValue;
+		next = myNext; }
+
+}
